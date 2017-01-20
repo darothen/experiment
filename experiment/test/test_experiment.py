cf4efde0e0d0080874675aaa3bea5ab7afde2aba
Daniel Rothenberg <darothen@mit.edu>
2017-01-20 10:36:38 -0500

    Adding some additional tests saved from marc_analysis package

--- experiment/test/test_experiment.py
+++ experiment/test/test_experiment.py
@@ -11,16 +11,19 @@ import os
 import unittest
 import yaml
 
+from itertools import product
 from experiment import Experiment, Case
 
 
+case_emis = \
+    Case('emis', 'Emissions Scenario', ['policy', 'no_policy', 'weak_policy'])
+case_model_config = \
+    Case('model_config', 'Model configuration',
+         ['no_clouds', 'no_sun', 'no_sun_no_clouds'])
+
 my_experiment = Experiment(
     name='my_experiment',
-    cases = [
-        Case('emis', 'Emissions Scenario', ['policy', 'no_policy', 'weak_policy']),
-        Case('model_config', 'Model configuration',
-             ['no_clouds', 'no_sun', 'no_sun_no_clouds']),
-    ],
+    cases = [case_emis, case_model_config],
     timeseries=True, data_dir='/path/to/my/data',
     case_path='{emis}/{model_config}',
     output_prefix='experiment_{emis}_{model_config}.data.',
@@ -38,6 +41,62 @@ class TestExperiment(unittest.TestCase):
         print(expected)
         print(actual)
         self.assertEqual(expected, actual)
+
+    def test_case_attrs(self):
+
+        case_list = [case_emis, case_model_config]
+
+        # Cases property
+        self.assertEqual(my_experiment.cases,
+                         [case.shortname for case in case_list])
+
+        # itercases generator
+        actual_cases = [(case.shortname, case.longname, case.vals)
+                        for case in case_list]
+        exp_cases = [case for case in my_experiment.itercases()]
+        self.assertEqual(exp_cases, actual_cases)
+
+        # all_cases list
+        actual_case_gen = product(*[case.vals for case in case_list])
+        exp_case_gen = my_experiment.all_cases()
+        for expected, actual in zip(exp_case_gen, actual_case_gen):
+            self.assertEqual(expected, actual)
+
+    def test_walk_cases_default(self):
+        """ Walk cases when no case_path is provided - so bits are
+        ordered in the order passed as cases """
+
+        case_list = [case_emis, case_model_config]
+        exp = my_experiment
+
+        actual_paths = []
+        for case_ordered in product(*[case.vals for case in case_list]):
+            actual_paths.append(os.path.join(*case_ordered))
+        exp_paths = exp._walk_cases()
+        for expected, actual in zip(exp_paths, actual_paths):
+            print(expected, actual)
+            self.assertEqual(expected, actual)
+
+    def test_walk_cases_custom(self):
+        """ Walk cases when no case_path is provided - so bits are
+        ordered in the order passed as cases """
+
+        case_list = [case_emis, case_model_config]
+        exp = my_experiment
+
+        actual_paths = []
+        for case_ordered in product(*[case.vals for case in case_list]):
+            actual_paths.append(os.path.join(*case_ordered))
+        exp_paths = exp._walk_cases()
+        for expected, actual in zip(exp_paths, actual_paths):
+            print(expected, actual)
+            self.assertEqual(expected, actual)
+
+        # Generic - no format arguments
+        exp = Experiment("Test Experiment", case_list,
+                         case_path="", validate_data=False)
+        for p in exp._walk_cases():
+            self.assertEqual(p, "")
 
     def test_validate(self):
         """ Test ability for Experiment to infer whether or not data corresponding
